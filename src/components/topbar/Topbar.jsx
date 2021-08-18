8fc606d109eeeba999c3693cc9fa9610d0d69865
kimnewbie <gracecancho@gmail.com>
2021-08-18 13:39:29 +0900

    finished

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,70 @@
+import {Link} from 'react-router-dom';
+import './topbar.css';
+
+export default function Topbar() {
+    const user = true;
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"/>
+                <i className="topIcon fab fa-twitter-square"/>
+                <i className="topIcon fab fa-pinterest-square"/>
+                <i className="topIcon fab fa-instagram-square"/>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">
+                        {/* style={{textDecoration:'none', color: 'inherit'}} */}
+                        <Link className="link" to="/">
+                            HOME
+                        </Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to="/about">
+                            ABOUT
+                        </Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to="/contact">
+                            CONTACT
+                        </Link>
+                    </li>
+                    <li className="topListItem">
+                        <Link className="link" to="/write">
+                            WRITE
+                        </Link>
+                    </li>
+                    <li className="topListItem">
+                        {user &&
+                            "LOG-OUT"
+                        }
+                    </li>
+                </ul>
+            </div>
+            <div className="topRight"> {/* 로그인 됐을 때만 사진 */}
+            { user ? (
+                <Link className="link" to="/settings">
+                    <img
+                        className="topImg"
+                        src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                        alt=""/>
+                </Link> 
+                ) : (
+                    <ul className="topList">
+                        <li className="topListItem">
+                            <Link className="link" to="/login">
+                                LOGIN
+                            </Link>
+                        </li>
+                        <li className="topListItem">
+                            <Link className="link" to="/register">
+                                REGISTER
+                            </Link>
+                        </li>                        
+                    </ul>
+                ) }
+                <i className="topSearchIcon fas fa-search"/>
+            </div>
+        </div>
+    )
+}
